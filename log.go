f73290443940d5b827c0b6bd652ce8fcc7c75d87
CrazyMax <crazy-max@users.noreply.github.com>
2019-06-08 21:58:43 +0200

    Fix

--- log.go
+++ log.go
@@ -111,9 +111,7 @@ type Level uint8
 
 const (
 	// TraceLevel defines trace log level.
-	TraceLevel Level = -1
-	// DebugLevel defines debug log level.
-	DebugLevel Level = iota
+	TraceLevel Level = iota
 	// InfoLevel defines info log level.
 	InfoLevel
 	// WarnLevel defines warn log level.
