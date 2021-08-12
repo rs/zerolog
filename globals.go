d92a906fcad9c1b4166e1a2e3d7321e19cdeece9
Sean <77011910+sean-rn@users.noreply.github.com>
2021-08-11 20:18:16 -0400

    Allow user configuration of which logger to return from Ctx() (#343)
    If a user is trying to fetch a logger from their context, they probably
    want to log something.  In order to allow returning a useable logger from Ctx()
    without breaking backwards compatibilty with the previous behavior, we make it
    configurable.

--- globals.go
+++ globals.go
@@ -100,6 +100,10 @@ var (
 	// output. If not set, an error is printed on the stderr. This handler must
 	// be thread safe and non-blocking.
 	ErrorHandler func(err error)
+
+	// DefaultContextLogger is returned from Ctx() if there is no logger associated
+	// with the context.
+	DefaultContextLogger *Logger
 )
 
 var (
