075b768faf7b7d69016491b50ca667d702e6e8e6
Edward McFarlane <edward.mcfarlane@outlook.com>
2021-10-01 01:29:20 +0100

    TestWriter set better frame zero default with t.Helper

--- writer.go
+++ writer.go
@@ -107,6 +107,7 @@ func MultiLevelWriter(writers ...io.Writer) LevelWriter {
 type TestingLog interface {
 	Log(args ...interface{})
 	Logf(format string, args ...interface{})
+	Helper()
 }
 
 // TestWriter is a writer that writes to testing.TB.
@@ -119,11 +120,13 @@ type TestWriter struct {
 
 // NewTestWriter creates a writer that logs to the testing.TB.
 func NewTestWriter(t TestingLog) TestWriter {
-	return TestWriter{T: t, Frame: 1}
+	return TestWriter{T: t}
 }
 
 // Write to testing.TB.
 func (t TestWriter) Write(p []byte) (n int, err error) {
+	t.T.Helper()
+
 	n = len(p)
 
 	// Strip trailing newline because t.Log always adds one.
@@ -131,8 +134,8 @@ func (t TestWriter) Write(p []byte) (n int, err error) {
 
 	// Try to correct the log file and line number to the caller.
 	if t.Frame > 0 {
-		_, origFile, origLine, _ := runtime.Caller(0)
-		_, frameFile, frameLine, ok := runtime.Caller(t.Frame)
+		_, origFile, origLine, _ := runtime.Caller(1)
+		_, frameFile, frameLine, ok := runtime.Caller(1 + t.Frame)
 		if ok {
 			erase := strings.Repeat("\b", len(path.Base(origFile))+len(strconv.Itoa(origLine))+3)
 			t.T.Logf("%s%s:%d: %s", erase, path.Base(frameFile), frameLine, p)
@@ -147,6 +150,6 @@ func (t TestWriter) Write(p []byte) (n int, err error) {
 // ConsoleTestWriter creates an option that correctly sets the file frame depth for testing.TB log.
 func ConsoleTestWriter(t testing.TB) func(w *ConsoleWriter) {
 	return func(w *ConsoleWriter) {
-		w.Out = TestWriter{T: t, Frame: 7}
+		w.Out = TestWriter{T: t, Frame: 6}
 	}
 }
