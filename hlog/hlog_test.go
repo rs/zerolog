dfd20718fa993a32aac810552026f5120f7af069
Mitar <mitar.git@tnode.com>
2023-10-23 11:08:31 +0200

    Add TestGetHost.

--- hlog/hlog_test.go
+++ hlog/hlog_test.go
@@ -408,3 +408,28 @@ func TestHostHandlerWithoutPort(t *testing.T) {
 		t.Errorf("Invalid log output, got: %s, want: %s", got, want)
 	}
 }
+
+func TestGetHost(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"", ""},
+		{"example.com:8080", "example.com"},
+		{"example.com", "example.com"},
+		{"invalid", "invalid"},
+		{"192.168.0.1:8080", "192.168.0.1"},
+		{"[2001:0db8:85a3:0000:0000:8a2e:0370:7334]:8080", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"},
+		{"こんにちは.com:8080", "こんにちは.com"},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.input, func(t *testing.T) {
+			result := getHost(tt.input)
+			if tt.expected != result {
+				t.Errorf("Invalid log output, got: %s, want: %s", result, tt.expected)
+			}
+		})
+	}
+}
