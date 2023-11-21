c1cdcd2394e971f67b44bf37a27cd31a3c2a7c06
dillonstreator <dillonstreator@gmail.com>
2023-11-21 13:08:16 -0600

    atomically read

--- hlog/internal/mutil/body.go
+++ hlog/internal/mutil/body.go
@@ -7,7 +7,7 @@ import (
 
 type byteCountReadCloser struct {
 	rc   io.ReadCloser
-	read int64
+	read *int64
 }
 
 var _ io.ReadCloser = (*byteCountReadCloser)(nil)
@@ -21,7 +21,7 @@ func NewByteCountReadCloser(rc io.ReadCloser) *byteCountReadCloser {
 
 func (b *byteCountReadCloser) Read(p []byte) (int, error) {
 	n, err := b.rc.Read(p)
-	atomic.AddInt64(&b.read, int64(n))
+	atomic.AddInt64(b.read, int64(n))
 	return n, err
 }
 
@@ -31,10 +31,10 @@ func (b *byteCountReadCloser) Close() error {
 
 func (b *byteCountReadCloser) WriteTo(w io.Writer) (int64, error) {
 	n, err := io.Copy(w, b.rc)
-	atomic.AddInt64(&b.read, n)
+	atomic.AddInt64(b.read, n)
 	return n, err
 }
 
 func (b *byteCountReadCloser) BytesRead() int64 {
-	return b.read
+	return atomic.LoadInt64(b.read)
 }
