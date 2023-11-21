6df9ed3e771a02a1c422c3f0f416930864c5cf2e
dillonstreator <dillonstreator@gmail.com>
2023-11-21 12:17:55 -0600

    implement io.WriterTo on byteCountReadCloser

--- hlog/internal/mutil/body.go
+++ hlog/internal/mutil/body.go
@@ -11,6 +11,7 @@ type byteCountReadCloser struct {
 }
 
 var _ io.ReadCloser = (*byteCountReadCloser)(nil)
+var _ io.WriterTo = (*byteCountReadCloser)(nil)
 
 func NewByteCountReadCloser(rc io.ReadCloser) *byteCountReadCloser {
 	return &byteCountReadCloser{
@@ -26,6 +27,12 @@ func (b *byteCountReadCloser) Read(p []byte) (int, error) {
 
 func (b *byteCountReadCloser) Close() error {
 	return b.rc.Close()
+}
+
+func (b *byteCountReadCloser) WriteTo(w io.Writer) (int64, error) {
+	n, err := io.Copy(w, b.rc)
+	atomic.AddInt64(&b.read, n)
+	return n, err
 }
 
 func (b *byteCountReadCloser) BytesRead() int64 {
