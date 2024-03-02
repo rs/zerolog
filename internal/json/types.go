bda298df4a4bf5c42292968b3cf3f6c68fd66766
Joseph Cumines <joeycumines@gmail.com>
2024-03-02 10:49:38 +1000

    Fix JSON encoding of float exponents to be like json.Marshal / ES6 (#537)
    * Fix JSON encoding of float exponents to be like json.Marshal / ES6
    * Add test cases for the *e-9 JSON number encoding edge case

--- internal/json/types.go
+++ internal/json/types.go
@@ -311,7 +311,25 @@ func appendFloat(dst []byte, val float64, bitSize int) []byte {
 	case math.IsInf(val, -1):
 		return append(dst, `"-Inf"`...)
 	}
-	return strconv.AppendFloat(dst, val, 'f', -1, bitSize)
+	// convert as if by es6 number to string conversion
+	// see also https://cs.opensource.google/go/go/+/refs/tags/go1.20.3:src/encoding/json/encode.go;l=573
+	strFmt := byte('f')
+	// Use float32 comparisons for underlying float32 value to get precise cutoffs right.
+	if abs := math.Abs(val); abs != 0 {
+		if bitSize == 64 && (abs < 1e-6 || abs >= 1e21) || bitSize == 32 && (float32(abs) < 1e-6 || float32(abs) >= 1e21) {
+			strFmt = 'e'
+		}
+	}
+	dst = strconv.AppendFloat(dst, val, strFmt, -1, bitSize)
+	if strFmt == 'e' {
+		// Clean up e-09 to e-9
+		n := len(dst)
+		if n >= 4 && dst[n-4] == 'e' && dst[n-3] == '-' && dst[n-2] == '0' {
+			dst[n-2] = dst[n-1]
+			dst = dst[:n-1]
+		}
+	}
+	return dst
 }
 
 // AppendFloat32 converts the input float32 to a string and
