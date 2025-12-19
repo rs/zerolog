bc07058071b050d5a20694019c9d06d964a1ff21
Marc Brooks <IDisposable@gmail.com>
2025-12-19 01:49:14 -0600

    Made diode tests more stable.

--- diode/diode_test.go
+++ diode/diode_test.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"sync"
 	"testing"
 	"time"
 
@@ -60,14 +61,22 @@ func TestFatal(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	slurp, err := io.ReadAll(stderr)
-	if err != nil {
-		t.Fatal(err)
-	}
+
+	var stderrBuf bytes.Buffer
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		_, _ = io.Copy(&stderrBuf, stderr)
+	}()
+
 	err = cmd.Wait()
 	if err == nil {
 		t.Error("Expected log.Fatal to exit with non-zero status")
 	}
+
+	wg.Wait() // Wait for the goroutine to finish copying
+	slurp := stderrBuf.Bytes()
 
 	want := "{\"level\":\"fatal\",\"message\":\"test\"}\n"
 	got := cbor.DecodeIfBinaryToString(slurp)
@@ -112,14 +121,22 @@ func TestFatalWithFilteredLevelWriter(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	slurp, err := io.ReadAll(stdout)
-	if err != nil {
-		t.Fatal(err)
-	}
+
+	var stdoutBuf bytes.Buffer
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		_, _ = io.Copy(&stdoutBuf, stdout)
+	}()
+
 	err = cmd.Wait()
 	if err == nil {
 		t.Error("Expected log.Fatal to exit with non-zero status")
 	}
+
+	wg.Wait() // Wait for the goroutine to finish copying
+	slurp := stdoutBuf.Bytes()
 
 	got := cbor.DecodeIfBinaryToString(slurp)
 	want := "{\"level\":\"fatal\",\"message\":\"test\"}\n"
